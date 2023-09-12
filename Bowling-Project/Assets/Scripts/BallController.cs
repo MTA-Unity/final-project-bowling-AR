612e758a5b18a9782045707bf205ab5ce3ecf4f0
Eyal Politansky <100349000+EyalPoly@users.noreply.github.com>
2023-09-12 21:17:20 +0300

    Ball movement, rotation and throw

--- Bowling-Project/Assets/Scripts/BallController.cs
+++ Bowling-Project/Assets/Scripts/BallController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -6,24 +7,28 @@ public class BallController : MonoBehaviour
     [SerializeField] private Rigidbody ballRigidBody;
     [SerializeField] private Transform ballTransform;
     [SerializeField] private Transform ballStartPosition;
-    private Transform _arrow;
+    [SerializeField] private ArrowController arrow;
     
-    private float _horizontalMovementSpeed = 4f;
-    private bool _ballIsMoving;
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float startThrowForce;
+
     private BallControlState _ballControlState;
     
-    private Vector3 _newPosition;
     private Vector3 _newRotation;
+    private Vector3 _newPosition;
+    private const float MinRotationDegrees = -90f;
+    private const float MaxRotationDegrees = 90f;
     
     private void Start()
     {
-        ballTransform = ballStartPosition;
+        ballTransform.position = ballStartPosition.position;
         _newPosition = ballTransform.position;
         _newRotation = ballTransform.rotation.eulerAngles;
 
         _ballControlState = BallControlState.Position;
     }
-    
+
     private void Update()
     {
         // Continue to next ball control state
@@ -35,42 +40,66 @@ public class BallController : MonoBehaviour
             }
             else
             {
-                // Change to next ball control state
-                _ballControlState = (BallControlState)((int)_ballControlState + 1);
+                // Calculate next ball control state
+                Debug.Log("Last control state: " + _ballControlState);
+            
+                int nextBallState = ((int)_ballControlState + 1) % Enum.GetValues(typeof(BallControlState)).Length;
+                _ballControlState = (BallControlState)nextBallState;
+            
+                Debug.Log("Current control state: " + _ballControlState);
             }
         }
-        
+
+        switch (_ballControlState)
+        {
+            case BallControlState.Position:
+            {
+                float horizontalInput = Input.GetAxis("Horizontal");
+                float horizontalMovementDelta = horizontalInput * movementSpeed * Time.deltaTime;
+                _newPosition += ballTransform.TransformDirection(Vector3.right) * horizontalMovementDelta;
+                break;
+            }
+            case BallControlState.Rotation:
+            {
+                float rotationInput = Input.GetAxis("Horizontal");
+                float newYRotation = Mathf.Clamp(_newRotation.y + (rotationInput * rotationSpeed * Time.deltaTime),
+                    MinRotationDegrees, MaxRotationDegrees);
+                _newRotation = Vector3.up * newYRotation;
+                ballTransform.rotation = Quaternion.Euler(_newRotation);
+                break;
+            }
+            case BallControlState.ThrowForce:
+            {
+                float verticalInput = Input.GetAxis("Vertical");
+                arrow.ChangeArrowScale(verticalInput);
+                break;
+            }
+        }
+    }
+    
+    void FixedUpdate()
+    {
         if (_ballControlState == BallControlState.Position)
         {
-            var horizontalInput = Input.GetAxis("Horizontal");
-            var horizontalMovementDelta = horizontalInput * _horizontalMovementSpeed * Time.deltaTime;
-            _newPosition = _newPosition + ballTransform.TransformDirection(Vector3.right) * horizontalMovementDelta;
+            ballRigidBody.MovePosition(_newPosition);
         }
-        
-
-        else if (_ballControlState == BallControlState.Rotation)
-        {
-            
-        }
-
-
-
-        // var rotationInput = Input.GetAxis("Mouse X");
-        // var rotationDelta = rotationInput * _rotationSpeed;
-        // _newRotation += Vector3.up * rotationDelta;
     }
 
     private void ThrowBall()
     {
-        throw new System.NotImplementedException();
-    }
+        arrow.gameObject.SetActive(false);
+        ballRigidBody.isKinematic = false;
+        
+        // Calculate the throw force based on the arrow's local scale on the Z-axis.
+        float throwForce = arrow.GetArrowScaleOnZAxis() * startThrowForce;
+        Vector3 throwForceVector = arrow.transform.forward * throwForce;
+        Debug.Log("throwForceVector: " + throwForceVector);
 
-    void FixedUpdate()
-    {
-        ballRigidBody.MovePosition(_newPosition);
-       // ballRigidBody.MoveRotation(Quaternion.Euler(_newRotation));
+        ballRigidBody.AddForce(throwForceVector, ForceMode.Impulse);
+        
+        // Reset the ball control state and arrow scale.
+        // arrow.ResetArrowScale();
     }
-
 }
 
 public enum BallControlState
